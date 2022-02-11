a204adf7ede7cce3674433b663d4ab954063334b
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-11 19:40:46 +0300

    first

---
+++ 1-BasicInputValidations/1-BasicInputValidations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-BasicInputValidations
+//
+//  Created by ugur-pc on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
