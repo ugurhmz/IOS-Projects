fa039b36d3d3a0a680dcb978f576924c41f71d13
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-12 02:53:56 +0300

    Sending data with performSegue & prepare

---
+++ 3-SegueTransitionListening/3-SegueTransitionListening/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  3-SegueTransitionListening
+//
+//  Created by ugur-pc on 12.02.2022.
+//
+
+import Foundation
+
+
+class Person {
+    
+    var personId : Int?
+    var name : String?
+    
+    
+    init(id : Int, name : String) {
+        self.personId = id
+        self.name = name
+    }
+    
+    
+}
--- 3-SegueTransitionListening/3-SegueTransitionListening/ViewController.swift
+++ 3-SegueTransitionListening/3-SegueTransitionListening/ViewController.swift
@@ -5,9 +5,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func clickGoto2(_ sender: Any) {
+        
+        let myPerson = Person(id: 67, name : "Ugur")
+        
+        
+        // veri burdan  --->   prepare gönderiliyor
+        performSegue(withIdentifier: "gofrom1to2", sender : myPerson)
+    }
+    
+    
+    
+    // performSeguden geliyor...
+    override func prepare(for segue: UIStoryboardSegue, sender : Any?) {
+        
+        let comingData = sender as? Person
+        
+        print("Gelen data -> \(comingData!.personId!)     |    \(comingData!.name!)")
+        print("Ayrıca 1 -> 2 ")
+    }
+    
+    
 }
 
