5dde84c767438f4e0dcc9298bd35999b5bbfe261
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-11 20:22:56 +0300

    Basic input validations with sum calculation operation

--- 1-BasicInputValidations/1-BasicInputValidations/ViewController.swift
+++ 1-BasicInputValidations/1-BasicInputValidations/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  1-BasicInputValidations
-//
-//  Created by ugur-pc on 11.02.2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number2: UITextField!
+    @IBOutlet weak var number1: UITextField!
+  
+    @IBOutlet weak var topLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("qweq")
+        topLabel.text = ""
     }
 
 
+    @IBAction func changeBtn(_ sender: Any) {
+        
+        guard let text = number1.text, !number1.text!.isEmpty, !number2.text!.isEmpty else {
+                             
+                topLabel.text =  "Boş bırakalamaz!"
+                topLabel.font = UIFont(name:"Helvetica",size:32)
+                topLabel.textColor = .red
+                return
+                
+            }
+        
+        if let num1 = Int(number1.text!) {
+            if let num2 = Int(number2.text!) {
+                topLabel.textColor = .black
+                topLabel.text = "Result : \(num1 + num2)"
+            }else {
+                typeErr()
+            }
+            
+            
+            
+        } else {
+            typeErr()
+        }
+  
+        
+        func typeErr(){
+            topLabel.text = "Type Hatası!"
+            topLabel.font = UIFont(name:"Helvetica",size:25)
+            topLabel.textColor = .orange
+        }
+    }
 }
 
