71532fbf3a9871de0d19b89130671fabcb715d02
ugurhmz <craxx67@gmail.com>
2022-02-15 23:47:47 +0300

    Time Picker  created then  reverted the old view to all pickers

--- 12-Date Picker Usage/12-Date Picker Usage/ViewController.swift
+++ 12-Date Picker Usage/12-Date Picker Usage/ViewController.swift
@@ -10,27 +10,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var hoursTextField: UITextField!
     
     var datePicker:UIDatePicker?
-    
+    var timePicker : UIDatePicker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        datePicker = UIDatePicker()
-        datePicker?.datePickerMode = .date
+        //________________________DATE PICKER________________________
+            datePicker = UIDatePicker()
+            datePicker?.datePickerMode = .date
+            
+            // Klasik görünüme çevirme
+            if #available(iOS 13.4, *) {
+                datePicker?.preferredDatePickerStyle = .wheels
+            }
+            dateTextField.inputView = datePicker
+            datePicker?.addTarget(self, action: #selector(self.showDate(datePicker:)), for: .valueChanged)
         
-        dateTextField.inputView = datePicker
+        //________________________ /DATE PICKER________________________
         
-        datePicker?.addTarget(self, action: #selector(self.showDate(datePicker:)), for: .valueChanged)
+        
+        
+        
+        
+        
+        //________________________ TIME PICKER _________________________
+        
+            timePicker = UIDatePicker()
+            timePicker?.datePickerMode = .time
+            hoursTextField.inputView = timePicker
+            
+            if #available(iOS 13.4, *) {
+                timePicker?.preferredDatePickerStyle = .wheels
+            }
+        
+            timePicker?.addTarget(self, action: #selector(self.showTime(timePicker:)),  for: .valueChanged)
+            
+            
+        //________________________/TIME PICKER _________________________
+        
+        
+        
+        // BOŞA TIKLAYINCA KAYBOLSUN.
         let gestureRecog = UITapGestureRecognizer(target: self, action: #selector(self.gestureMetod))
-        
+        view.addGestureRecognizer(gestureRecog)
     }
     
     
     
-    // showDate
+    // showDate   -> Date
     @objc func showDate(datePicker:UIDatePicker) {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/yyyy"
+        dateFormatter.dateFormat = "dd/MM/yyyy"
         
         let getDate = dateFormatter.string(from: datePicker.date)
         dateTextField.text = getDate
@@ -40,10 +70,25 @@ class ViewController: UIViewController {
     }
 
     
-    // gestureMetod
+    // gestureMetod    -> Date
     @objc func gestureMetod() {
         view.endEditing(true)
     }
+    
+    
+    // showTimePicker   -> Time
+    @objc func showTime(timePicker:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm"
+        
+        let getTime = dateFormatter.string(from: timePicker.date)
+        hoursTextField.text = getTime
+    }
+    
+    
+    
+    
+    
 
 }
 
