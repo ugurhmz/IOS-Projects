f3c94daa9e363d0b009fe67ff5274d0dbe5f1425
ugurhmz <craxx67@gmail.com>
2022-02-16 21:40:03 +0300

    UIPickerView & UIToolbar usage

--- 13- Textfield with Picker View/13- Textfield with Picker View/ViewController.swift
+++ 13- Textfield with Picker View/13- Textfield with Picker View/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     var pickerView:UIPickerView?
     var mycities = [String]()
-    
+    var selectedCity :  String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,35 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         pickerView?.delegate = self
         
         myField.inputView = pickerView
+        
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.tintColor = UIColor.blue
+        toolBar.sizeToFit()
+        
+        let okBtn = UIBarButtonItem(title : "OK", style: .plain, target: self, action: #selector(self.okClicked))
+        let spaceBetweenBtn = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelBtn = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(self.cancelClicked))
+        
+        
+        toolBar.setItems([cancelBtn, spaceBetweenBtn, okBtn], animated: true)
+        myField.inputAccessoryView = toolBar
+        
+    }
+    
+    // OK
+    @objc func okClicked(){
+        myField.text = self.selectedCity
+        print(selectedCity)
+        view.endEditing(true)
+    }
+    
+    
+    // Cancel
+    @objc func cancelClicked() {
+        myField.text = ""
+        view.endEditing(true)
     }
     
     
@@ -54,6 +83,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         myField.text = mycities[row]
+        self.selectedCity = mycities[row]
     }
     
 
