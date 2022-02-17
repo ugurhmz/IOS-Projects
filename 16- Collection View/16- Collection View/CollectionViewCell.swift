25d4105e628fd48dc71e85c86a695e6e69b663b3
ugurhmz <craxx67@gmail.com>
2022-02-17 17:59:41 +0300

    All datas listed in Collectin View

---
+++ 16- Collection View/16- Collection View/CollectionViewCell.swift
@@ -0,0 +1,8 @@
+
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+}
--- 16- Collection View/16- Collection View/ViewController.swift
+++ 16- Collection View/16- Collection View/ViewController.swift
@@ -3,11 +3,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var fruitCollections: UICollectionView!
+    
+    var fruits:[String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fruits = ["Cherry","Pear","Apple","Grape","Sour Cherry","Fig","Blueberries","Plum"]
+        
+        fruitCollections.delegate = self
+        fruitCollections.dataSource = self
         
     }
 
 
 }
 
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mycell", for: indexPath) as! CollectionViewCell
+        
+        // Cell'in -> içindeki labelin -> Texti.
+        cell.cellLabel.text = "\(self.fruits[indexPath.row])"
+        
+        return cell
+    }
+    
+    
+}
